9385f12bba55abd841692eca70f38e763c2f3a10
Stephia <stephaniefhatida@hotmail.com>
2024-08-20 09:50:17 -0300

    a

---
+++ Lista1/Ex3/Exercicio.php
@@ -0,0 +1,8 @@
+Criar um script PHP, que apresente o valor da multa a ser paga pelo motorista,
+crie uma variável para armazenar a velocidade máxima permitida em uma avenida e
+a velocidade com que o motorista estava dirigindo nela.
+Sabendo que são pagos:
+a) 50 reais se o motorista estiver ultrapassar em até 10% a velocidade permitida
+(ex.: velocidade máxima: 50km/h; motorista a até 55km/h);
+b) 100 reais, se o motorista ultrapassar de 10 a 20% a velocidade permitida.
+c) 200 reais, se estiver acima de 20% da velocidade permitida.
