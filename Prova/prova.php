ac76bf4a565ace3c1ddd0bd36a95707cdee2ab28
Stephia <stephaniefhatida@hotmail.com>
2024-08-27 10:05:32 -0300

    a

---
+++ Prova/prova.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+Crie um script PHP que receba a seguinte frase: “Esta prova de PHP esta molezinha.”,
+transforme a frase em letras maiúsculas, exiba qual é caractere que está na posição do meio da frase.
+Troque todas as vogais pela letra X e exiba quantidade caracteres que não são vogais.
+Crie uma nova frase a partir da letra ”d” e exiba a frase de forma inversa.
